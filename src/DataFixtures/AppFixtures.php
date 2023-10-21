dc0e639239746a123e4a10bd9442d044638cf341
Kim <kimberley.quenon.@gmail.com>
2023-10-21 15:50:31 +0200

    fixtures et bdd

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Car;
+use Cocur\Slugify\Slugify;
+use Faker\Provider\Fakecar;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+        $faker->addProvider(new Fakecar($faker)); //appel au faker de voiture
+        $slugify = new Slugify(); //init d'un nouveau slug hors boucle! sinon bouclé x fois et perte d'optimisation
+
+        for ($i=1; $i <= 30 ; $i++) { 
+            $car = new Car();
+            $model = $faker->vehicleModel;
+            $brand = $faker->vehicleBrand;
+            $coverImage = $faker->imageUrl(1000,400);
+            $carburant = $faker->vehicleFuelType;
+            $year = $faker->biasedNumberBetween(1990, date('Y'), 'sqrt');
+            $transmission = $faker->vehicleGearBoxType;
+            $content= '<p>'.join('</p><p>', $faker->paragraphs(2)).'</p>';
+            $options= '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>';
+
+            $car->setModel($model)
+                ->setBrand($brand)
+                ->setContent($content)
+                ->setCoverImage($coverImage)
+                ->setKm(rand(0,80000))
+                ->setPrice(rand(15000,50000))
+                ->setOwners(rand(1,3))
+                ->setCylinder(rand(1,12))
+                ->setPower(rand(80,500))
+                ->setCarburant($carburant)
+                ->setYear($year)
+                ->setTransmission($transmission)
+                ->setContent($content)
+                ->setOptions($options);
+
+            $manager->persist($car);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ src/Entity/Car.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace App\Entity;
+
+use Cocur\Slugify\Slugify;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CarRepository;
+
+#[ORM\Entity(repositoryClass: CarRepository::class)]
+#[ORM\HasLifecycleCallbacks]
+class Car
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $model = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $brand = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $coverImage = null;
+
+    #[ORM\Column]
+    private ?int $km = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $owners = null;
+
+    #[ORM\Column]
+    private ?float $cylinder = null;
+
+    #[ORM\Column]
+    private ?int $power = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $carburant = null;
+
+    #[ORM\Column]
+    private ?int $year = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $transmission = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $options = null;
+
+    /**
+     * init "slugification"
+     *
+     * @return void
+     */
+    #[ORM\PrePersist]
+    #[ORM\PreUpdate]
+    public function initializeSlug(): void
+    {
+        if(empty($this->slug))
+        {
+            $slugify = new Slugify();
+            //slugifier la marque et le modèle pour créer un slug "combiné"
+            $brandSlug = $slugify->slugify($this->brand);
+            $modelSlug = $slugify->slugify($this->model);
+
+            $this->slug = $brandSlug . '-' . $modelSlug;
+        }
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): static
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): static
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getCoverImage(): ?string
+    {
+        return $this->coverImage;
+    }
+
+    public function setCoverImage(string $coverImage): static
+    {
+        $this->coverImage = $coverImage;
+
+        return $this;
+    }
+
+    public function getKm(): ?int
+    {
+        return $this->km;
+    }
+
+    public function setKm(int $km): static
+    {
+        $this->km = $km;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getOwners(): ?int
+    {
+        return $this->owners;
+    }
+
+    public function setOwners(int $owners): static
+    {
+        $this->owners = $owners;
+
+        return $this;
+    }
+
+    public function getCylinder(): ?float
+    {
+        return $this->cylinder;
+    }
+
+    public function setCylinder(float $cylinder): static
+    {
+        $this->cylinder = $cylinder;
+
+        return $this;
+    }
+
+    public function getPower(): ?int
+    {
+        return $this->power;
+    }
+
+    public function setPower(int $power): static
+    {
+        $this->power = $power;
+
+        return $this;
+    }
+
+    public function getCarburant(): ?string
+    {
+        return $this->carburant;
+    }
+
+    public function setCarburant(string $carburant): static
+    {
+        $this->carburant = $carburant;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): static
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getTransmission(): ?string
+    {
+        return $this->transmission;
+    }
+
+    public function setTransmission(string $transmission): static
+    {
+        $this->transmission = $transmission;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): static
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getOptions(): ?string
+    {
+        return $this->options;
+    }
+
+    public function setOptions(string $options): static
+    {
+        $this->options = $options;
+
+        return $this;
+    }
+}
