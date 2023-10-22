a18ad220e925a4219a0606e09ff3937a457f21d9
Kim <kimberley.quenon.@gmail.com>
2023-10-22 18:17:49 +0200

    edit user + confirm log out

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -83,6 +84,36 @@ class AccountController extends AbstractController
         }
 
         return $this->render("account/registration.html.twig",[
+            'myForm'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * Modifier le profil d'utilisateur
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/profile", name:"account_profile")]
+    public function profile(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser(); //récupère l'user connecté
+        $form = $this->createForm(AccountType::class, $user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+            'success',
+            'Les données ont été enregistrées avec succès'    
+            );
+        }
+
+        return $this->render("account/profile.html.twig",[
             'myForm'=>$form->createView()
         ]);
     }
---
+++ src/Form/AccountType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Form\ApplicationType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class AccountType extends ApplicationType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName', TextType::class, $this->getConfiguration("Prénom", "Votre prénom..."))
+            ->add('lastName', TextType::class, $this->getConfiguration("Nom", "Votre nom..."))
+            ->add('email', EmailType::class, $this->getConfiguration("Mail", "Votre adresse mail..."))
+            ->add('picture', UrlType::class, $this->getConfiguration("Photo de profil", "Url..."))
+            ->add('introduction', TextType::class, $this->getConfiguration("Introduction", "Votre intro..."))
+            ->add('description', TextareaType::class, $this->getConfiguration("Description", "Votre description..."))
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
