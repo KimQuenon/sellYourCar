aa3d3bf11631fbbadf58b2f5d932764850c326fc
Kim <kimberley.quenon.@gmail.com>
2023-10-22 17:03:00 +0200

    too many attempts

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -6,6 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 
 class AccountController extends AbstractController
     {
@@ -20,10 +21,19 @@ class AccountController extends AbstractController
     {
         $error = $utils->getLastAuthenticationError(); //récupérer la dernière erreur engendrée
         $username = $utils->getLastUsername(); //récupérer le dernier username à s'être connecté
+        $loginError = null; //init la var pour toomanyattempts
+
+        if($error instanceof TooManyLoginAttemptsAuthenticationException)
+        {
+            //l'erreur est due à la limitation de tentative de connexion
+            $loginError= "Trop de tentatives de connexion, réessayez plus tard...";
+
+        }
 
         return $this->render('account/index.html.twig', [
             'hasError' => $error !== null, //pas nul => afficher erreur
-            'username'=> $username
+            'username'=> $username,
+            'loginError'=> $loginError
         ]);
     }
 
